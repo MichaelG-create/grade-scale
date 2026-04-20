1dfa97646bbe69302aad36c281fb228416da99c8
Michael Garcia <michael.garcia.73@gmail.com>
2026-04-20 10:21:31 +0200

    chore: trigger first ci/cd run

--- infra/modules/static_web_app/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "api_key" {
-  value     = azurerm_static_web_app.main.api_key
-  sensitive = true
-}
-
-output "default_host_name" {
-  value = azurerm_static_web_app.main.default_host_name
-}
